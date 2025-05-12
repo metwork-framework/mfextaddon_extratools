cb8e89847f33042e449889813742f145907e3ebb
metworkbot <metworkbot@metwork-framework.org>
2025-05-12 16:39:30 +0000

    build: sync common files from github_organization_management repository

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# mfextaddon_template
-template repository for building mfext addons
+[![logo](https://raw.githubusercontent.com/metwork-framework/resources/master/logos/metwork-white-logo-small.png)](http://www.metwork-framework.org)
+# mfextaddon_extratools
+
+[//]: # (automatically generated from https://github.com/metwork-framework/github_organization_management/blob/master/common_files/README.md)
+
+**Status (master branch)**
+
+[![GitHub CI](https://github.com/metwork-framework/mfextaddon_extratools/actions/workflows/ci.yml/badge.svg?branch=master)](https://github.com/metwork-framework/mfextaddon_extratools/actions?query=workflow%3ACI+branch%3Amaster)
+[![Maintenance](https://raw.githubusercontent.com/metwork-framework/resources/master/badges/maintained.svg)](https://github.com/metwork-framework/resources/blob/master/badges/maintained.svg)
+[![Gitter](https://github.com/metwork-framework/resources/blob/master/badges/community-en.svg)](https://gitter.im/metwork-framework/community-en?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
+[![Gitter](https://github.com/metwork-framework/resources/blob/master/badges/community-fr.svg)](https://gitter.im/metwork-framework/community-fr?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge)
+
+
+
+## What is MetWork FrameWork?
+
+[MetWork Framework](https://metwork-framework.org) is an opensource system
+for building and managing production grade applications or micro-services
+(from development to operations).
+
+
+
+
+
+
+
+## Reference documentation
+
+- (for **master (development)** version), see [this dedicated site](http://metwork-framework.org/pub/metwork/continuous_integration/docs/master/mfextaddon_extratools/) for reference documentation.
+- (for **latest released stable** version), see [this dedicated site](http://metwork-framework.org/pub/metwork/releases/docs/stable/mfextaddon_extratools/) for reference documentation.
+
+For very specific use cases, you might be interested in
+[reference documentation for integration branch](http://metwork-framework.org/pub/metwork/continuous_integration/docs/integration/mfextaddon_extratools/).
+
+And if you are looking for an old released version, you can search [here](http://metwork-framework.org/pub/metwork/releases/docs/).
+
+
+
+
+## Installation guide
+
+See [this document](https://metwork-framework.org/pub/metwork/continuous_integration/docs/master/mfextaddon_extratools/100-installation_guide/).
+
+
+## Configuration guide
+
+See [this document](https://metwork-framework.org/pub/metwork/continuous_integration/docs/master/mfextaddon_extratools/300-configuration_guide/).
+
+
+
+## Contributing guide
+
+See [CONTRIBUTING.md](CONTRIBUTING.md) file.
+
+
+
+## Code of Conduct
+
+See [CODE_OF_CONDUCT.md](CODE_OF_CONDUCT.md) file.
+
+
+
+## Sponsors
+
+*(If you are officially paid to work on MetWork Framework, please contact us to add your company logo here!)*
+
+[![logo](https://raw.githubusercontent.com/metwork-framework/resources/master/sponsors/meteofrance-small.jpeg)](http://www.meteofrance.com)
---
+++ integration_tests/0096_system_extra_dependencies/test.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+
+OK_DEPS=$(cat list.txt|grep -v "#"|xargs)
+OK_NOT_FOUND=$(cat list_ok_not_found.txt|grep -v "#"|xargs)
+
+#N=$(cat /etc/redhat-release 2>/dev/null |grep -c "^CentOS release 6")
+#if test "${N}" -eq 0; then
+#    echo "We test this only on centos6"
+#    exit 0
+#fi
+
+export PATH=${PATH}:${PWD}/..
+RET=0
+
+
+cd "${MFEXT_HOME}" || exit 1
+cd opt || exit 1
+for layer in *; do
+    [[ -e $layer ]] || break
+    cd "${layer}" || exit 1
+    if test -f .mfextaddon; then
+        addon="mfextaddon_"$(cat .mfextaddon)
+        if test "mfextaddon_extratools" = "${addon}"; then
+            echo
+            echo "=== System extra dependencies layer ${layer} ==="
+            echo
+            current_layer=$(cat .layerapi2_label)
+            DEPS1=$(layer_wrapper --layers="${current_layer}" -- external_dependencies.sh |awk -F '/' '{print $NF}' |xargs)
+            # We don t consider libraries available in the layer (they should not be here, probably a LD_LIBRARY_PATH issue)
+            DEPS2=""
+            for lib in ${DEPS1}; do
+                found=$(find . -name "${lib}" -print)
+                if test "${found}" == ""; then
+                    DEPS2="${DEPS2} ${lib}"
+                fi
+            done
+            echo "--- external dependencies ---" "${DEPS2}"
+            for DEP in ${DEPS2}; do
+                FOUND=0
+                for OK_DEP in ${OK_DEPS}; do
+                    if test "${DEP}" = "${OK_DEP}"; then
+                        FOUND=1
+                        break
+                    fi
+                done
+                if test "${FOUND}" = "1"; then
+                    continue
+                fi
+                echo "***** ${DEP} *****"
+                echo "=== revert ldd layer ${layer} ==="
+                revert_ldd.sh "${DEP}"
+                echo
+                echo
+                RET=1
+            done
+            DEPS3=$(layer_wrapper --layers="${current_layer}" -- external_dependencies_not_found.sh |xargs)
+            # We don t consider libraries available in the layer (they should not be here, probably a LD_LIBRARY_PATH issue)
+            DEPS4=""
+            for lib in ${DEPS3}; do
+                found=$(find . -name "${lib}" -print)
+                if test "${found}" == ""; then
+                    DEPS4="${DEPS4} ${lib}"
+                fi
+            done
+            echo "--- dependencies not found ---" ${DEPS4}
+            for DEP in ${DEPS4}; do
+                FOUND=0
+                for OK_DEP in ${OK_NOT_FOUND}; do
+                    if test "${DEP}" = "${OK_DEP}"; then
+                        FOUND=1
+                        break
+                    fi
+                done
+                if test "${FOUND}" = "1"; then
+                    continue
+                fi
+                echo "***** ${DEP} *****"
+                echo "=== revert ldd not found ==="
+                revert_ldd_not_found.sh "${DEP}"
+                echo
+                echo
+                RET=1
+            done
+        fi
+    fi
+    cd ..
+done
+
+
+if test "${RET}" = "1"; then
+    echo "Failure of dependencies test"
+    exit 1
+fi
